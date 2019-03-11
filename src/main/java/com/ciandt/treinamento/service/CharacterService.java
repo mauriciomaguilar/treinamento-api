8873be768d68d07b101c5d1f0e92f62df9f663a6
Sarah Roland Campos Lisboa <sarahrc@ciandt.com>
2019-03-11 15:54:09 -0300

    fix to find all

--- src/main/java/com/ciandt/treinamento/service/CharacterService.java
+++ src/main/java/com/ciandt/treinamento/service/CharacterService.java
@@ -1,9 +1,13 @@
 package com.ciandt.treinamento.service;
 
+import java.util.List;
+
 import com.ciandt.treinamento.controller.entity.Personagem;
 import com.ciandt.treinamento.entity.PersonagemEntity;
 
 public interface CharacterService {
+
+	public List<Personagem> returnAllCharacters(Integer limit);
 
 	public PersonagemEntity search();
 
--- src/main/java/com/ciandt/treinamento/service/impl/CharacterServiceImpl.java
+++ src/main/java/com/ciandt/treinamento/service/impl/CharacterServiceImpl.java
@@ -3,27 +3,35 @@ package com.ciandt.treinamento.service.impl;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import com.ciandt.treinamento.constants.MarvelApiConstants;
 import com.ciandt.treinamento.controller.entity.Personagem;
 import com.ciandt.treinamento.dto.ResponseCharacterJson;
+import com.ciandt.treinamento.entity.PersonagemEntity;
+import com.ciandt.treinamento.service.CharacterService;
 import com.ciandt.treinamento.util.ApiUtils;
 
-public class CharacterServiceImpl  {
-	
-	@GetMapping(path = "/marvel", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public ResponseEntity<List<Personagem>> returnAllCharacters(@RequestParam(name = "limit", required = false) Integer limit) {
-		String url = ApiUtils.buildUrl(MarvelApiConstants.PATH_CHARACTERS,
-				MarvelApiConstants.PRIVATE_KEY, 
+public class CharacterServiceImpl implements CharacterService {
+
+	public List<Personagem> returnAllCharacters(Integer limit) {
+		String url = ApiUtils.buildUrl(MarvelApiConstants.PATH_CHARACTERS, MarvelApiConstants.PRIVATE_KEY,
 				MarvelApiConstants.API_KEY_VALUE);
-		ResponseCharacterJson personagens =  (ResponseCharacterJson) ApiUtils.getObject(url, ResponseCharacterJson.class);
+		ResponseCharacterJson personagens = (ResponseCharacterJson) ApiUtils.getObject(url,
+				ResponseCharacterJson.class);
 		List<Personagem> listaPersonagem = new ArrayList<>();
 		personagens.getData().getResults().stream().map(p -> listaPersonagem.add(p));
-		return ResponseEntity.ok(listaPersonagem);
-		
+		return listaPersonagem;
+
+	}
+
+	@Override
+	public PersonagemEntity search() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Personagem searchByName(String nome) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
