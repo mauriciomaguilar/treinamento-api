0831347c7c42b44f8a78d3d4f4c756e33943acde
Cassia Ramos <ritars@ciandt.com>
2019-03-11 11:37:29 -0300

    rota-busca-todos-personagens

---
+++ src/main/java/com/ciandt/treinamento/service/CharacterService.java
@@ -0,0 +1,12 @@
+package com.ciandt.treinamento.service;
+
+public class CharacterService {
+
+
+
+	public Object characterList(Long quantidade) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
