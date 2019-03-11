778050aae2965e0d0aae719c5dceda9d1ee2d4f4
diego-jose <diegokickfly@gmail.com>
2019-03-11 12:05:46 -0300

    Apagando classes de usuario

--- src/main/java/com/ciandt/treinamento/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ciandt.treinamento.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ciandt.treinamento.entity.UserEntity;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-
-}
