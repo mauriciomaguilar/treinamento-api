440c99ebb23c9ad26c5bc058aa0cbcabdee3b3bc
Sarah Roland Campos Lisboa <sarahrc@ciandt.com>
2019-03-11 15:37:37 -0300

    BuscaPor nome

--- src/main/java/com/ciandt/treinamento/service/CharacterService.java
+++ src/main/java/com/ciandt/treinamento/service/CharacterService.java
@@ -1,8 +1,11 @@
 package com.ciandt.treinamento.service;
 
+import com.ciandt.treinamento.controller.entity.Personagem;
 import com.ciandt.treinamento.entity.PersonagemEntity;
 
 public interface CharacterService {
 
 	public PersonagemEntity search();
+
+	public Personagem searchByName(String nome);
 }
