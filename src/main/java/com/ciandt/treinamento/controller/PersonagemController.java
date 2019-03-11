31a307f8f9ee60e7e78fd56e5111699dbd2f7e37
Felipe Henrique Leal <felipehl@ciandt.com>
2019-03-11 14:26:15 -0300

    Included search by id function

---
+++ src/main/java/com/ciandt/treinamento/controller/PersonagemController.java
@@ -0,0 +1,28 @@
+package com.ciandt.treinamento.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ciandt.treinamento.entity.PersonagemEntity;
+import com.ciandt.treinamento.service.CharacterService;
+
+@RequestMapping("api/v1")
+public class PersonagemController{
+	
+	private CharacterService service;
+		
+	@Autowired
+	public PersonagemController(CharacterService service) {
+		this.service = service;
+	}
+
+	@GetMapping("/characters/{id}")
+	public ResponseEntity<PersonagemEntity> getOneCharacterById(@PathVariable Long id) {
+		PersonagemEntity personagem;
+		return null;
+	}
+}
--- src/main/java/com/ciandt/treinamento/service/CharacterService.java
+++ src/main/java/com/ciandt/treinamento/service/CharacterService.java
@@ -1,8 +1,12 @@
 package com.ciandt.treinamento.service;
 
+import com.ciandt.treinamento.controller.entity.Personagem;
 import com.ciandt.treinamento.entity.PersonagemEntity;
 
 public interface CharacterService {
 
-	public PersonagemEntity search();
+	public PersonagemEntity searchAll();
+	
+	public Personagem searchById();
+	
 }
