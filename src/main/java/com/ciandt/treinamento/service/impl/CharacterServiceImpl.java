81520c26d0f523e6b1a1c84314846ccbf6fa3bab
Sarah Roland Campos Lisboa <sarahrc@ciandt.com>
2019-03-11 15:58:32 -0300

    fix service

--- src/main/java/com/ciandt/treinamento/service/impl/CharacterServiceImpl.java
+++ src/main/java/com/ciandt/treinamento/service/impl/CharacterServiceImpl.java
@@ -13,14 +13,7 @@ import com.ciandt.treinamento.util.ApiUtils;
 public class CharacterServiceImpl implements CharacterService {
 
 	public List<Personagem> returnAllCharacters(Integer limit) {
-		String url = ApiUtils.buildUrl(MarvelApiConstants.PATH_CHARACTERS, MarvelApiConstants.PRIVATE_KEY,
-				MarvelApiConstants.API_KEY_VALUE);
-		ResponseCharacterJson personagens = (ResponseCharacterJson) ApiUtils.getObject(url,
-				ResponseCharacterJson.class);
-		List<Personagem> listaPersonagem = new ArrayList<>();
-		personagens.getData().getResults().stream().map(p -> listaPersonagem.add(p));
-		return listaPersonagem;
-
+		return findAll();
 	}
 
 	@Override
@@ -33,5 +26,15 @@ public class CharacterServiceImpl implements CharacterService {
 	public Personagem searchByName(String nome) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public List<Personagem> findAll() {
+		String url = ApiUtils.buildUrl(MarvelApiConstants.PATH_CHARACTERS, MarvelApiConstants.PRIVATE_KEY,
+				MarvelApiConstants.API_KEY_VALUE);
+		ResponseCharacterJson personagens = (ResponseCharacterJson) ApiUtils.getObject(url,
+				ResponseCharacterJson.class);
+		List<Personagem> listaPersonagem = new ArrayList<>();
+		personagens.getData().getResults().stream().map(p -> listaPersonagem.add(p));
+		return listaPersonagem;
 	}
 }
